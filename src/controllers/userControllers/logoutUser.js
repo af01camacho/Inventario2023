6d680a85edb83c509502b1d73d2bc0d96bdd6fc5
Andres <af01camacho@gmail.com>
2023-12-02 11:01:07 -0500

    update

--- src/controllers/userControllers/logoutUser.js
+++
@@ -1,4 +0,0 @@
-export const logoutUser =  (req, res) => {
-    res.cookie('token', '')
-    return res.status(200).json({ message: '¡Cerraste sesion exitosamente!' })
-}
--- src/database.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const ConexionDB = async () => {
-        const DB = mongoose.connect(process.env.DB)
-        await DB 
-        ? console.log('*** CONEXION EXITOSA ***')
-        : console.log('***ERROR DE LA BASE DE DATOS')
-}
